b626299385db92e91dd4f3b0948ca5ad09c73640
Никита Алексеев <viktorifoks2004@mail.ru>
2026-05-13 01:31:35 +0300

    patern FAN out finished

--- ITK_Code/cmd/main.go
+++ ITK_Code/cmd/main.go
@@ -1,29 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"runtime"
+	"sync"
 )
 
-func Level1() {
-	defer func() {
-		err := recover()
+const FilePath string = "../files/"
+
+func worker(id int, jobs <-chan string, results chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for fileName := range jobs {
+
+		fileDirectory := fmt.Sprintf("%s%s", FilePath, fileName)
+		text, err := os.ReadFile(fileDirectory)
 		if err != nil {
-			fmt.Println("Паника обработана на уровне 1: ошибка в Level3")
+			fmt.Printf("file %s is not valid error: %v", fileDirectory, err)
 		}
-	}()
-	Level2()
-}
 
-func Level2() {
-	Level3()
-	defer fmt.Println("Завершаем Level2")
-}
+		countWordsInFiles := len(bytes.Fields(text))
 
-func Level3() {
-	panic("ошибка в Level3")
+		results <- fmt.Sprintf("File name: %s Count worlds in file: %d", fileName, countWordsInFiles)
+	}
 }
 
 func main() {
+	files, err := os.ReadDir(FilePath)
+	if err != nil {
+		fmt.Printf("directory is not valid: %v", err)
+	}
+	workers := runtime.NumCPU()
 
-	Level1()
+	wg := sync.WaitGroup{}
+
+	results := make(chan string)
+	jobs := make(chan string)
+
+	go func() {
+		defer close(jobs)
+		for _, f := range files {
+
+			jobs <- f.Name()
+		}
+	}()
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go worker(i, jobs, results, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for n := range results {
+		fmt.Println(n)
+	}
+
 }
