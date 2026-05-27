79af487490ae4c9069a87abf275a7cc83ed17dd4
Никита Алексеев <viktorifoks2004@mail.ru>
2026-05-27 14:56:47 +0300

    import interceptor

--- ITK_Code/UserApi/cmd/server/main.go
+++ ITK_Code/UserApi/cmd/server/main.go
@@ -5,7 +5,11 @@ import (
 	"log"
 	"net"
 
+	commonInterceptor "github.com/Truncklin/exchange-common/interceptor"
+	metrics "github.com/Truncklin/exchange-common/metrics"
 	pb "github.com/Truncklin/exchange-contract/generated"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"go.uber.org/zap"
 
 	"google.golang.org/grpc"
 )
@@ -15,7 +19,17 @@ func main() {
 	if err != nil {
 		log.Fatal("error listen port 50051: ", err)
 	}
-	grpcServer := grpc.NewServer()
+
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				commonInterceptor.RequestID(),
+				commonInterceptor.NewLogger(&zap.Logger{}).Unary(),
+				commonInterceptor.Recovery(),
+				metrics.Prometheus(),
+			),
+		),
+	)
 	userService := handler.NewUserService()
 	pb.RegisterUserServiceServer(
 		grpcServer,
