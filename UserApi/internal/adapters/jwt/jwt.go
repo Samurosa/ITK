c67eb2d58123ece4e94e36f5dd58ef25fa0a15fe
samurosa <75kno15co10s@gmail.com>
2026-06-28 02:29:17 +0300

    Token generation has been implemented.

--- UserApi/internal/adapters/jwt/jwt.go
+++ UserApi/internal/adapters/jwt/jwt.go
@@ -2,12 +2,8 @@ package jwt
 
 import (
 	"ITK_Code/m/v2/internal/core/auth"
-	"ITK_Code/m/v2/internal/core/user"
 	"ITK_Code/m/v2/internal/core/user/models"
-	"context"
 	"time"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 type JWT struct {
@@ -18,13 +14,8 @@ func NewJWT(jwtConfig auth.JWTConfig) *JWT {
 	return &JWT{jwtConfig: jwtConfig}
 }
 
-func (j *JWT) GenerateTokens(ctx context.Context, user models.User, deviceID string) (auth.TokensModel, error) {
+func (j *JWT) GenerateTokens(user models.User) (auth.TokensModel, error) {
 	refreshTokenString, err := generateRefreshToken()
-	if err != nil {
-		return auth.TokensModel{}, err
-	}
-
-	refreshTokenHash, err := bcrypt.GenerateFromPassword([]byte(refreshTokenString), bcrypt.DefaultCost)
 	if err != nil {
 		return auth.TokensModel{}, err
 	}
@@ -41,9 +32,4 @@ func (j *JWT) GenerateTokens(ctx context.Context, user models.User, deviceID str
 		AccessExpiresAt:  time.Now().Add(j.jwtConfig.AccessTokenTTL),
 		RefreshExpiresAt: time.Now().Add(j.jwtConfig.RefreshTokenTTL),
 	}, nil
-}
-
-func (j *JWT) RefreshAccessToken(ctx context.Context) (auth.TokensModel, error) {
-
-	return auth.TokensModel{}, nil
 }
