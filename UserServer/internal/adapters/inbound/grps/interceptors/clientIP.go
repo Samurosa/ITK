76876d00bb089f688f0ce85692e216ab8903185b
Samurosa <75kno15co10s@gmail.com>
2026-08-05 02:37:50 +0300

    remake ip interceptor, addition func receiving ip from grpc metadata

--- UserServer/internal/adapters/inbound/grps/interceptors/clientIP.go
+++ UserServer/internal/adapters/inbound/grps/interceptors/clientIP.go
@@ -9,38 +9,50 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 )
 
 func ClientIPInterceptor(log *zap.Logger) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context,
+	return func(
+		ctx context.Context,
 		req interface{},
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
-	) (
-		interface{},
-		error,
-	) {
-		log = log.Named("Rate limiting interceptor")
-		value, ok := peer.FromContext(ctx)
+	) (interface{}, error) {
+
+		log := log.Named("client-ip-interceptor")
+
+		md, ok := metadata.FromIncomingContext(ctx)
 		if !ok {
-			log.Error("No peer info found", zap.String("value", value.String()))
-			return nil, status.Error(codes.NotFound, "Client IP address is missing")
+			log.Info("ip from metadata not found")
 		}
 
-		host, _, err := net.SplitHostPort(value.Addr.String())
-		if err != nil {
-			log.Error("failed to split host and port", zap.Error(err))
-			return nil, status.Error(codes.InvalidArgument, "Client IP address is invalid")
+		ip := md.Get("x-forwarded-for")[0]
+
+		if ip == "" {
+			p, ok := peer.FromContext(ctx)
+			if !ok {
+				log.Error("peer not found")
+				return nil, status.Error(codes.Internal, "client ip not found")
+			}
+
+			host, _, err := net.SplitHostPort(p.Addr.String())
+			if err != nil {
+				log.Error("invalid peer address", zap.Error(err))
+				return nil, status.Error(codes.Internal, "invalid client ip")
+			}
+			log.Info("peer address received")
+
+			ip = host
 		}
 
-		ctx, err = reqCtx.UpdateRequestContext(ctx,
-			func(baseContext *dto.RequestContext) {
-				baseContext.Metadata.ClientIP = host
-			})
+		ctx, err := reqCtx.UpdateRequestContext(ctx, func(rc *dto.RequestContext) {
+			rc.Metadata.ClientIP = ip
+		})
 		if err != nil {
-			log.Info("failed to update request context", zap.Error(err))
+			log.Error("update request context", zap.Error(err))
 			return nil, status.Error(codes.Internal, "failed to update request context")
 		}
 
